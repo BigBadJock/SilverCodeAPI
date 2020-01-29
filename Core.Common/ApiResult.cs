d03acb2aa1fdc7585af47bda655d1f399f06bd51
John McArthur <john.mcarthur@silvercode.co.uk>
2020-01-29 00:23:19 +0000

    Added ApiResult and Pagination

---
+++ Core.Common/ApiResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Common
+{
+    public class ApiResult<T>
+    {
+        public IEnumerable<T> Data { get; set; }
+        public Pagination? Pagination { get; set; }
+    }
+}
---
+++ Core.Common/Pagination.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Common
+{
+    public class Pagination
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        public int TotalCount { get; set; }
+    }
+}
