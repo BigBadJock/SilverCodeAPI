41b0562a83be30ccbdcf6e1aca6dd29c7baf879c
John McArthur <john.mcarthur@silvercode.co.uk>
2020-04-21 23:48:02 +0100

    Added BaseDataService and IDataService Bumped version

---
+++ Core.Common.Contracts/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using REST_Parser.Models;
+using System.Threading.Tasks;
+
+namespace Core.Common.Contracts
+{
+    public interface IDataService<T> where T : class, IModel, new()
+    {
+        Task<T> Add(T model);
+        Task<T> Update(T model);
+        Task<bool> Delete(T model);
+        Task<T> Get(int id);
+        RestResult<T> Search(string restQuery);
+        
+    }
+}
---
+++ Core.Common/BaseDataService.cs
@@ -0,0 +1,41 @@
+﻿using Core.Common.Contracts;
+using Microsoft.Extensions.Logging;
+using REST_Parser.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace Core.Common
+{
+    public class BaseDataService<T> : IDataService<T> where T : class, IModel, new()
+    {
+        protected IRepository<T> repository;
+        protected ILogger<T> logger;
+
+
+        public async Task<T> Add(T model)
+        {
+            return await this.repository.Add(model);
+        }
+
+        public async Task<bool> Delete(T model)
+        {
+            return await this.repository.Delete(model);
+        }
+
+        public async Task<T> Get(int id)
+        {
+            return await this.repository.GetById(id);
+        }
+
+        public RestResult<T> Search(string restQuery)
+        {
+            this.logger.LogInformation(string.Format("searching using restQuery {0}", restQuery));
+            return this.repository.GetAll(restQuery);
+        }
+
+        public Task<T> Update(T model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
