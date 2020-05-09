3a9e91a77c24ce0f5ea148f8bd6620269d62af3f
John McArthur <john.mcarthur@silvercode.co.uk>
2020-05-09 01:24:35 +0100

    Updated BaseModel with comments and examples for swagger

--- Core.Common.DataModels/BaseModel.cs
+++ Core.Common.DataModels/BaseModel.cs
@@ -6,14 +6,35 @@ namespace Core.Common.DataModels
 {
     public abstract class BaseModel : IModel
     {
+        /// <summary>
+        /// Unique Id
+        /// </summary>
+        /// <example>1</example>
         [Key]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Delete flag
+        /// </summary>
+        /// <example>false</example>
         [Editable(false)]
         public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Date & Time entity created
+        /// </summary>
         [Editable(false)]
         public DateTime Created { get; set; }
+
+        /// <summary>
+        /// Date & Time entity last updated
+        /// </summary>
         [Editable(false)]
         public DateTime LastUpdated { get; set; }
+
+        /// <summary>
+        /// user name of last updater
+        /// </summary>
         [Editable(false)]
         public string LastUpdatedBy { get; set; }
 
