274bec3f22b45779d31666e4d689d0ff15c669ee
John McArthur <john.mcarthur@silvercode.co.uk>
2025-01-25 19:52:26 +0000

    Remove custom claim and refresh token functionality
    This commit deletes the ICustomClaimTypeRepository and
    IRefreshTokenRepository interfaces, along with their
    associated namespaces. Additionally, the CustomClaimType
    and RefreshToken classes have been removed, indicating a
    significant refactoring of the codebase related to these
    functionalities.

--- Core.Common.DataModels/CustomClaimType.cs
+++
@@ -1,9 +0,0 @@
-﻿using Core.Common.DataModels.Interfaces;
-
-namespace Core.Common.DataModels
-{
-    public class CustomClaimType : BaseModelWithIntId, ILookupModel
-    {
-        public string Name { get; set; }
-    }
-}
